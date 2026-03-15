2f41d6157eb9c7eefc95d5663868b21d6a5938b1
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2026-03-16 00:43:37 +0600

    hotel booking frontend

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import "./index.css";
 import AppRoutes from "./routes/AppRoutes.jsx";
-import { AuthProvider } from "./Context/AuthContext.jsx";
+import { AuthProvider } from "./context/AuthContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
