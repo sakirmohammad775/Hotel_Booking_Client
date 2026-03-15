9eae98924e46a09da7c3a8d2dc8e60ec7289c48b
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2026-03-15 23:35:02 +0600

    add new backend url

--- src/services/Api-Client.js
+++ src/services/Api-Client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "https://hotel-booking-website-gamma.vercel.app/api/v1/",
+  baseURL: "https://hotel-booking-website-mocha.vercel.app/api/v1/",
 });
 
 export default apiClient;
